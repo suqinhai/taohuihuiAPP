d6dfa2423fed179ccc317c0cd9b34f41b5410edb
suqinhai <suqinhai@vsoontech.com>
2017-08-02 17:12:39 +0800

    修改

--- src/components/common/poster.vue
+++ src/components/common/poster.vue
@@ -1,20 +1,10 @@
 <template>
-   <!-- 菜单 -->
-    <!-- <nav>
-      <li v-for="n in navList">
-        <router-link :to="{name: 'classPage', query: { columnId: n.id, name: n.name }}">
-          <a href="javascript:viod(0)"><img :src="n.url"><span>{{n.name}}</span></a>
-        </router-link>
-      </li>
-    </nav> -->
-
     <!-- 轮播 -->
-    <div :class="imgClass"  @click="bigSlider">
-      <div class="swipe">
-        <mt-swipe :auto="auto" :defaultIndex="index">
-          <mt-swipe-item v-for="m in navPoster"><img v-lazy="m.img" width="100%" height="100%"></mt-swipe-item>
-        </mt-swipe>
-      </div>
+ 
+    <div class="swipe">
+      <mt-swipe :auto="auto" :defaultIndex="index">
+        <mt-swipe-item v-for="m in navPoster"><img v-lazy="m.img" width="100%" height="100%"></mt-swipe-item>
+      </mt-swipe>
     </div>
 
 </template>
@@ -23,7 +13,9 @@
   export default {
     data () {
         return {
-          navPoster:[]
+          navPoster:[],
+          auto:4000,
+          index:1,
         }
     },
     mounted(){
@@ -39,5 +31,5 @@
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped="scoped">
-@import './scss/nav.scss';
+@import './scss/poster.scss';
 </style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,44 +1,30 @@
 <template>
   <div id="content" class="content" v-title data-title="好惠买">
-    
-
     <!-- 首页轮播 -->
     <su-poster></su-poster>
-    
     <!-- 搜索 -->
     <div class="search" v-show="!search">
-        <div class="searchText" @click="searchText">好多优惠等来你搜</div>
+      <div class="searchText" @click="searchText">好多优惠等来你搜</div>
     </div>
-
     <div class="search" v-show="search">
       <div class="inputWarp">
-        <input autofocus type="text" name="search" placeholder="好多优惠等来你搜"
-        v-model="searchInputText"
-        @keyup.13="onEnter" 
-        @keyup="keyup">
+        <input autofocus type="text" name="search" placeholder="好多优惠等来你搜" v-model="searchInputText" @keyup.13="onEnter" @keyup="keyup">
         <a href="###" class="close" v-show="close" @click="empty"></a>
       </div>
       <span><a href="###" @click="cancel">取消</a></span>
     </div>
-    
-    
-
     <!-- 菜单 -->
     <su-nav></su-nav>
-    
     <!-- 底部导航菜单 -->
     <su-bottomMenu></su-bottomMenu>
-
-   <!--  每日精选 -->
-
-    <section class="itemWarp" v-infinite-scroll="loadMore"
-    infinite-scroll-disabled="loading"
-    infinite-scroll-distance="100">
-      <div class="title"><span></span><font>每日精选</font><span></span></div>
+    <!--  每日精选 -->
+    <section class="itemWarp" v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="100">
+      <div class="title"><span></span>
+        <font>每日精选</font><span></span></div>
       <div class="itemContent">
         <div class="itemList" v-for="i in itemList">
-        <!-- <a :href="'/details?_id=' + i._id"> -->
-         <router-link :to="{name: 'details', query: { _id: i._id,title: i.title}}"> 
+          <!-- <a :href="'/details?_id=' + i._id"> -->
+          <router-link :to="{name: 'details', query: { _id: i._id,title: i.title}}">
             <!-- 商品图片 -->
             <div v-if="i.pictUrl">
               <img src="" v-lazy="i.pictUrl">
@@ -46,7 +32,6 @@
             <div v-else>
               <img src="" v-lazy="m.showUrl" v-for="m in i.mainImgJson" v-if="m.selected">
             </div>
-            
             <div class="pd15">
               <div class="itemTitle text-overflow">{{ i.title }}</div>
               <div class="itemCoupon">
@@ -59,121 +44,122 @@
               </div>
             </div>
           </router-link>
-         <!--  </a> -->
+          <!--  </a> -->
         </div>
-       
       </div>
-    </section>  
-
-     <!--  loading -->
+    </section>
+    <!--  loading -->
     <div v-if="showLoading" style="width:25px;margin:0 auto">
-        <mt-spinner type="fading-circle" :size="25"></mt-spinner>
+      <mt-spinner type="fading-circle" :size="25"></mt-spinner>
     </div>
     <div v-else style="text-align: center">——抱歉，没有更多商品啦——</div>
-
     <!-- 遮罩 -->
-  <!-- <su-shade></su-shade>  -->
+    <!-- <su-shade></su-shade>  -->
+
+    <!-- 回到顶部 -->
+    <div id="top" @click="goTop"></div>
   </div>
 </template>
-
 <script>
 import suBottomMenu from './../../components/common/bottomMenu'
 import suNav from './../../components/common/nav'
 import suPoster from './../../components/common/poster'
-export default{
-    data () {
-        return {
-           msg:'首页',
-           show:true,
-           itemList:[],
-           page:1,
-           pageSize:6,
-           search:false,
-           searchInputText:'',
-           close:false,
-           showLoading:true,
-        }
+export default {
+  data() {
+    return {
+      msg: '首页',
+      show: true,
+      itemList: [],
+      page: 1,
+      pageSize: 6,
+      search: false,
+      searchInputText: '',
+      close: false,
+      showLoading: true,
+    }
+  },
+  mounted: function() {
+
+  },
+
+  methods: {
+    goTop(){
+      document.body.scrollTop = 0
     },
-    mounted:function() {
-    
+    searchText() {
+      this.search = true
     },
 
-    methods:{
+    // 取消按钮
+    cancel() {
+      this.searchInputText = ''
+      this.search = false
+    },
 
-      searchText() {
-        this.search = true
-      },
+    // 是否显示关闭按钮
+    keyup() {
+      if (this.searchInputText) {
+        this.close = true
+      } else {
+        this.close = false
+      }
+    },
 
-      // 取消按钮
-      cancel() {
-        this.searchInputText = ''
-        this.search = false
-      },
+    // 清空Input
+    empty() {
+      this.searchInputText = ''
+    },
 
-      // 是否显示关闭按钮
-      keyup() {
-        if ( this.searchInputText ){
-           this.close = true
-        }else{
-           this.close = false
-        }
-      },
+    //按回车搜索
+    onEnter() {
+      if (!this.searchInputText) { return false }
+      this.$router.push({ name: 'searchPage', query: { name: this.searchInputText } })
+    },
 
-      // 清空Input
-      empty() {
-        this.searchInputText = ''
-      },
+    loadMore() {
+      let _this = this
 
-      //按回车搜索
-      onEnter() {
-          if (!this.searchInputText) {return false}
-          this.$router.push({ name: 'searchPage', query: { name: this.searchInputText }})
-      },
+      if (!_this.showLoading || _this.loading) {
+        return false;
+      }
+      this.loading = true
+      setTimeout(() => {
 
-      loadMore() {
-        let _this = this
+        let url = 'taohuihui/frontend/goods/getItem?page=' + _this.page + '&pageSize=' + _this.pageSize
+        _this.axios.get(url).then(res => {
 
-        if ( !_this.showLoading || _this.loading ) {
-          return false;
-        }
-        this.loading = true
-        setTimeout(() => {
-          
-          let url = 'taohuihui/frontend/goods/getItem?page='+ _this.page +'&pageSize='+ _this.pageSize
-          _this.axios.get(url).then(res => {
+          //有数据的时候
+          if (res.data.list && res.data.list.length) {
+            _this.itemList = _this.itemList.concat(res.data.list)
+            _this.page++
+              _this.loading = false
 
-            //有数据的时候
-            if ( res.data.list && res.data.list.length){
-               _this.itemList = _this.itemList.concat(res.data.list)
-               _this.page ++
-               _this.loading = false
-
-               // 判断是不是少于最大pageSize，少于就是以后没有数据了
-               if (res.data.list.length <  _this.pageSize ){
-                   _this.showLoading = false;
-               }
-               
-            }else{
-               _this.showLoading = false;
+            // 判断是不是少于最大pageSize，少于就是以后没有数据了
+            if (res.data.list.length < _this.pageSize) {
+              _this.showLoading = false;
             }
 
-          }, res => {
-            // error callback
-          });
+          } else {
+            _this.showLoading = false;
+          }
 
-        }, 1000);
-        
-      },
+        }, res => {
+          // error callback
+        });
+
+      }, 1000);
+
     },
-    components:{
-      suBottomMenu,
-      suNav,
-      suPoster
-    }
+  },
+  components: {
+    suBottomMenu,
+    suNav,
+    suPoster
+  }
 }
+
 </script>
-
-
 <style rel="stylesheet/scss" lang="scss" scoped="scoped">
 @import './scss/index.scss';
+
 </style>
